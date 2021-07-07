a2a23538a9e30e3435879b1806ae74e67f97ea31
smilevent <1028330144@qq.com>
2021-07-07 20:24:21 +0800

    add github links

--- src/components/TitleBar.vue
+++ src/components/TitleBar.vue
@@ -1,21 +1,32 @@
 <template>
     <div class="title-bar">
         <div class="font-control">
-            <span @click="font_toggle">
+            <span @click="font_toggle" title="加载字体">
                 <Icon :color="fontButtonColor">
                     <TextFont20Filled />
                 </Icon>
+            </span>
+            <span>
+                <a
+                    href="https://github.com/liuxsdev/fanfou-poetry-website"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="源码"
+                >
+                    <Icon> <Github /> </Icon
+                ></a>
             </span>
         </div>
     </div>
 </template>
 <script>
 import { TextFont20Filled } from "@vicons/fluent";
+import { Github } from "@vicons/fa";
 import { Icon } from "@vicons/utils";
 
 export default {
     name: "TitleBar",
-    components: { TextFont20Filled, Icon },
+    components: { TextFont20Filled, Icon, Github },
     data() {
         return {
             font_is_active:
@@ -45,5 +56,11 @@ export default {
 <style scoped>
 span {
     cursor: pointer;
+    margin-right: 0.2em;
+}
+
+span a {
+    color: #1b1d1d;
+    text-decoration: none;
 }
 </style>
